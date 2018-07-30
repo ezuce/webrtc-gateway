da5447bacbbb489224877521637339ecdfe4cf1c
Martin Harcar <martin.harcar@ezuce.com>
2018-07-30 07:23:17 -0400

    Restart automatically gateway when crash

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 . ./docker_name.sh
-sudo docker run -d -p 7088:7088 -p 7089:7089 -p 8088:8088 -p 8089:8089 -p 30000-31000:30000-31000/udp $DOCKER_IMAGE_NAME -S stun.l.google.com:19302 -e
+sudo docker run -d --restart=unless-stopped -p 7088:7088 -p 7089:7089 -p 8088:8088 -p 8089:8089 -p 30000-31000:30000-31000/udp $DOCKER_IMAGE_NAME -S stun.l.google.com:19302 -e
